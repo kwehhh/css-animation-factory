3f3fcfd146fbe26dace17eff8efea44f83cb7a01
PowerGlove <john.yagiz@gmail.com>
2020-12-06 18:05:48 -0800

    Dev Mode Installed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # css-animation-factory
 CSS Animation Factory
+
+DEV SERVED AT:
+```
+http://localhost:8080/
+```
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      templateContent: '<div id="container"></div>'
+    })
+  ]
+};
